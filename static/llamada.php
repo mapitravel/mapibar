ff88da39be6b71c7265793af548c4f280d3f5938
yury <yfhq.12@gmail.com>
2013-06-21 19:44:33 -0500

    ventana de llamar

---
+++ static/llamada.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Llame ahora gratis</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.2.0/pure-min.css">
+    <link rel="stylesheet" href="llamada.css">
+</head>
+<body>
+    <div class="content pure-g-r">
+        <div class="header pure-u-1 align-center">
+            <h1><i class="icon icon-phone"></i> ¡Llame ahora gratis!</h1>
+        </div>
+        <div class="body pure-u-1">
+            <form class="pure-form pure-form-aligned">
+                <div class="pure-control-group">
+                    <label for="nombres">Nombres:</label>
+                    <input type="text" name="nombres" id="nombres" class="pure-input-1-2">
+                </div>
+                <div class="pure-control-group">
+                    <label for="telefono">Teléfono:</label>
+                    <input type="text" name="telefono" id="telefono" class="pure-input-1-2" placeholder="Ejemplo: +52 166 41856266">
+                </div>
+                <div class="pure-controls">
+                    <button type="submit" class="pure-button pure-button-primary">Llámenme ahora mismo</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
